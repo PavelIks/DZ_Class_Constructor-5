6220c0d7d560ed6011c63a984c019fef80ce241f
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-09-01 21:56:45 +0300

    Updated-6.

--- CPPFifthAPP.cpp
+++ CPPFifthAPP.cpp
@@ -5,12 +5,14 @@ int main()
 {
     setlocale(LC_ALL, "Russian");
 
-    const int size = 3;
+    const int size = 5;
     C_User* arr[size];
 
-    arr[0] = new C_User((char*)"KukuA-1", (char*)"KukuB-1", 1);
-    arr[1] = new C_User((char*)"KukuA-2", (char*)"KukuB-2", 2);
-    arr[2] = new C_User((char*)"KukuA-3", (char*)"KukuB-3", 3);
+    arr[0] = new C_User((char*)"Натюре", (char*)"Прохет", 19);
+    arr[1] = new C_User((char*)"Генерал", (char*)"Сасисов", 19);
+    arr[2] = new C_User((char*)"Капитан", (char*)"Колбасов", 56);
+    arr[3] = new C_User((char*)"Джонни", (char*)"Повар", 3);
+    arr[4] = new C_User((char*)"Император", (char*)"Вантузов", 12);
 
     for (int i = 0; i < size; i++)
     {
