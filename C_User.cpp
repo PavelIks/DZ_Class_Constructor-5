204f0d33da72d17007caeac853bbf3ccd9c81694
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-09-01 21:46:33 +0300

    Updated-5.

--- C_User.cpp
+++ C_User.cpp
@@ -1,6 +1,7 @@
 #include "C_User.h"
 #include <iostream>
 #include <string>
+#pragma warning(disable:4996) // *
 C_User::C_User()
 {
 	this->name = this->surname = (char*)"Я ХЗ кто ты или шо ты :-)";
@@ -10,13 +11,21 @@ C_User::C_User()
 std::string C_User::Convert_to_String()
 {
 	std::string s = std::to_string(age);
-	return s + "\n" + name + "\n" + surname + "\n";
+	return s + ", " + name + ", " + surname + ".\n";
 }
 
 C_User::C_User(char* name, char* surname, short age)
 {
-	this->name = name;
-	this->surname = surname;
+	/*this->name = name;
+	this->surname = surname;*/
+
+
+	this->name = new char[strlen(name) + 1]; // *
+	strcpy(this->name, name); // *
+
+	this->surname = new char[strlen(surname) + 1]; // *
+	strcpy(this->surname, surname); // *
+
 	this->age = age;
 }
 
