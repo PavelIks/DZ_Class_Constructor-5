c6cf67b28ed3906404433fa771d4fb563550eb10
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-09-01 19:38:27 +0300

    Updated-3.

--- CPPFifthAPP.cpp
+++ CPPFifthAPP.cpp
@@ -5,17 +5,15 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 
-	int size = 2;
-	C_User* arr = new C_User[size];
+        const int size = 3;
+        C_User* arr[size];
 
-	for (int i = 0; i < size; i++)
-	{
-		arr[i] = C_User((char*)"KukuA-1", (char*)"KukuB-1", 1);
-		arr[i] = C_User((char*)"KukuA-2", (char*)"KukuB-2", 2);
-		arr[i] = C_User((char*)"KukuA-3", (char*)"KukuB-3", 3);
-	}
+        arr[0] = new C_User((char*)"KukuA-1", (char*)"KukuB-1", 1);
+        arr[1] = new C_User((char*)"KukuA-2", (char*)"KukuB-2", 2);
+        arr[2] = new C_User((char*)"KukuA-3", (char*)"KukuB-3", 3);
 
-	std::cout << arr->Convert_to_String();
-
-	delete[]arr;
+        for (int i = 0; i < size; i++)
+            std::cout << arr[i]->Convert_to_String();
+        for (int i = 0; i < size; i++)
+            delete arr[i];
 }
